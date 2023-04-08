b81fe8181e39a08ba9e450d279126d5bd4a1f09a
JakubDz4 <dzadzkuba@gmail.com>
2023-04-08 17:06:53 +0200

    Added colliders and balon destroyed effect.

---
+++ Assets/Resources/Balloons/DestroyBalloon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBalloon : MonoBehaviour
+{
+
+    public float fallspeed = 1;
+    public float DestroyLifeTime = 10;
+    bool destroyed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            this.GetComponent<Animator>().Play("Destruction");
+            destroyed = true;
+            print("Collision");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(destroyed)
+        {
+            this.transform.Translate(0, -fallspeed, 0);
+            DestroyLifeTime -= Time.deltaTime;
+            if(DestroyLifeTime<0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}

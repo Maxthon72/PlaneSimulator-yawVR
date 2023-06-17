d0f3724a34b08eb3c16836be952e80cf9e4e43c1
Jolanta Lachman <jolalac346@student.polsl.pl>
2023-06-17 22:00:14 +0200

    Final terrain noise set up

--- Assets/TerrainGen/Scripts/Data/MeshSettings.cs
+++ Assets/TerrainGen/Scripts/Data/MeshSettings.cs
@@ -10,7 +10,7 @@ public class MeshSettings : UpdatableData
     public const int numSupportedFlatshadedChunkSizes = 3;
     public static readonly int[] supportedChunkSizes = { 48, 72, 96, 120, 144, 168, 192, 216, 240 };
 
-    public float meshScale = 2.5f;
+    public float meshScale = 6f;
     public bool useFlatShading;
 
     [Range(0, numSupportedChunkSizes - 1)]

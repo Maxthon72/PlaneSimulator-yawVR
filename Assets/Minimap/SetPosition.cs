fde7d570ff7ddd1755b08b61e45600ce7f7304a3
Jolanta Lachman <jolalac346@student.polsl.pl>
2023-06-27 15:05:40 +0200

    More improvements

--- Assets/Minimap/SetPosition.cs
+++ Assets/Minimap/SetPosition.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class DisableRotation : MonoBehaviour
 {
     public Transform player;
+
+    
+    public bool isBallon;
+    public Transform plane;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         Vector3 pos = player.transform.position;
-        pos.y += 15;
+        pos.y += 50;
         this.transform.position = pos;
+        if(isBallon)
+            this.transform.localEulerAngles = new Vector3(90, plane.transform.eulerAngles.y, 0);
+        else
+            this.transform.localEulerAngles = new Vector3(90, player.transform.eulerAngles.y, 0);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         Vector3 pos = player.transform.position;
-        pos.y += 10;
+        pos.y += 50;
         this.transform.position = pos;
+        if (isBallon)
+            this.transform.localEulerAngles = new Vector3(90, plane.transform.eulerAngles.y, 0);
+        else
+            this.transform.localEulerAngles = new Vector3(90, player.transform.eulerAngles.y, 0);
     }
 }
